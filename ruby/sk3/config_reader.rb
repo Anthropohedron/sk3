b1997a6420a3fad2039d6339c2beb664a133e7d1
Anthropohedron <Anthropohedron@users.noreply.github.com>
2017-06-14 17:05:53 -0400

    YAML configuration instead of a Ruby DSL

--- ruby/sk3/config_reader.rb
+++ ruby/sk3/config_reader.rb
@@ -1,122 +1,17 @@
+require "sk3/ext/split_logger"
+require "sk3/task"
+require "sk3/machine"
+require "sk3/demand"
+require "sk3/commit_task"
+require "sk3/commit_demand"
+require "sk3/ext/communicating_task"
+require "sk3/ext/communicating_demand"
 require "rubygems"
-require "blankslate"
+require "yaml"
 
 module SK3
 
-  class EvalObject < BlankSlate
-    def initialize(&block)
-      meta = (class << self; self; end)
-      meta.send(:define_method, :method_missing, &block)
-    end
-  end
-
-  class EvalReader
-
-    def initialize(event_queue, tasks, machines, demands)
-      @event_queue = event_queue
-      @tasks = tasks
-      @machines = machines
-      @demands = demands
-      @task_class = Task
-      @machine_class = Machine
-      @demand_class = Demand
-    end
-
-    def logger(_logger)
-      @event_queue.logger = _logger
-    end
-
-    def task_class(klass)
-      @task_class = klass
-    end
-
-    def machine_class(klass)
-      @machine_class = klass
-    end
-
-    def demand_class(klass)
-      @demand_class = klass
-    end
-
-    def tasks(&block)
-      event_queue = @event_queue
-      task_map = @tasks
-      task_class = @task_class
-      EvalObject.new { |name,options|
-        name = name.to_s
-        fail "Task name collision: #{name.inspect}" if task_map[name]
-        batch_size = SK3.get_positive_quantity(options, :batch_size)
-        batch_time = begin
-                       SK3.get_positive_time(options, :batch_time)
-                     rescue
-                       batch_size * SK3.get_positive_time(options, :unit_time)
-                     end
-        buffer = -SK3.get_positive_quantity(options, [ :deficit, :debit ], 0)
-        task_map[name] = task_class.new(event_queue, name,
-                                  batch_time, batch_size, buffer)
-      }.instance_eval(&block)
-      nil
-    end
-
-    def machines(&block)
-      event_queue = @event_queue
-      task_map = @tasks
-      machine_map = @machines
-      machine_class = @machine_class
-      EvalObject.new { |name,*args|
-        name = name.to_s
-        machine = (machine_map[name] ||= machine_class.new(event_queue, name))
-        args = args.map { |n| n.to_s }.sort.uniq.map { |task_name|
-          task = task_map[task_name]
-          fail "Unknown task: #{task_name.inspect}" unless task
-          if task.machine
-            if task.machine == machine
-              # already have it
-              nil
-            else
-              fail "Task #{task_name.inspect} is assigned to machine #{task.machine.name.inspect} and cannot be assigned to machine #{name.inspect}"
-            end
-          else
-            # all is well
-            task
-          end
-        }.compact
-        machine.add_tasks(*args) unless args.empty?
-      }.instance_eval(&block)
-      nil
-    end
-
-    def demands(&block)
-      event_queue = @event_queue
-      task_map = @tasks
-      demand_map = @demands
-      demand_class = @demand_class
-      EvalObject.new { |source_name,options|
-        source_name = source_name.to_s
-        source = task_map[source_name]
-        fail "No such task: #{source_name.inspect}" unless source
-        quantity = SK3.get_positive_quantity(options, :quantity)
-        sink_name = options[:to]
-        if sink_name && sink_name != :external
-          sink = task_map[sink_name.to_s]
-          fail "No such task: #{sink_name.inspect}" unless sink
-          sink.add_prereq(source, quantity)
-        else
-          if demand_map[source_name]
-            fail "Duplicate demand for #{source_name.inspect}"
-          end
-          interval = SK3.get_positive_time(options, :interval)
-          offset = SK3.get_positive_time(options, :offset, 0)
-          if offset > interval
-            fail "Offset (#{offset}) must be less than or equal to interval (#{interval})"
-          end
-          demand_map[source_name] =
-            demand_class.new(event_queue, source, quantity, interval, offset)
-        end
-      }.instance_eval(&block)
-    end
-
-  end
+  SK3 = self
 
   class ConfigReader
     attr_reader :event_queue, :tasks, :machines, :demands
@@ -135,9 +30,99 @@ module SK3
       @demands  = {}
     end
 
+    BaseLogConfig = { "dir" => "/tmp" }
+    BaseVarConfig = {
+      "task"    => "Task",
+      "machine" => "Machine",
+      "demand"  => "Demand"
+    }
+
+    module Util
+
+      def resolve(path, top=SK3)
+        begin
+          parts = path.split('::')
+          if parts[0] == ''
+            top = Object
+            parts.shift
+          end
+          parts.reduce(top) { |parent,name|
+            parent.const_get(name)
+          }
+        rescue
+          if top == Object
+            fail "Not found: #{path}"
+          else
+            resolve(path, Object)
+          end
+        end
+      end
+
+      extend self
+
+    end
+
     def read_config(config)
-      EvalReader.new(@event_queue,
-                     @tasks, @machines, @demands).instance_eval(config)
+      cfg = YAML.load(config)
+      logcfg = BaseLogConfig.merge(cfg["logger"] || {})
+      varcfg = BaseVarConfig.merge(cfg["variants"] || {})
+      @event_queue.logger = SplitLogger.new(logcfg["split"], logcfg["dir"])
+      task_class    = Util.resolve(varcfg["task"])
+      machine_class = Util.resolve(varcfg["machine"])
+      demand_class  = Util.resolve(varcfg["demand"])
+      cfg["tasks"].each do |name,options|
+        batch_size = SK3.get_positive_quantity(options, :batch_size)
+        batch_time = begin
+                       SK3.get_positive_time(options, :batch_time)
+                     rescue
+                       batch_size * SK3.get_positive_time(options, :unit_time)
+                     end
+        buffer = -SK3.get_positive_quantity(options, [ :deficit, :debit ], 0)
+        @tasks[name] = task_class.new(@event_queue, name,
+                                      batch_time, batch_size, buffer)
+      end
+      cfg["machines"].each do |name,tasklist|
+        machine = machine_class.new(@event_queue, name)
+        @machines[name] = machine
+        args = tasklist.map { |n| n.to_s }.sort.uniq.map { |task_name|
+          task = @tasks[task_name]
+          fail "Unknown task: #{task_name.inspect}" unless task
+          if task.machine
+            if task.machine == machine
+              # already have it
+              nil
+            else
+              fail "Task #{task_name.inspect} is assigned to machine #{task.machine.name.inspect} and cannot be assigned to machine #{name.inspect}"
+            end
+          else
+            # all is well
+            task
+          end
+        }.compact
+        machine.add_tasks(*args) unless args.empty?
+      end
+      cfg["demands"].each do |name,options|
+        source = @tasks[name]
+        fail "No such task: #{name.inspect}" unless source
+        quantity = SK3.get_positive_quantity(options, :quantity)
+        sink_name = options[:destination] || options["destination"]
+        if sink_name && sink_name != :external
+          sink = @tasks[sink_name.to_s]
+          fail "No such task: #{sink_name.inspect}" unless sink
+          sink.add_prereq(source, quantity)
+        else
+          if @demands[name]
+            fail "Duplicate demand for #{name.inspect}"
+          end
+          interval = SK3.get_positive_time(options, :interval)
+          offset = SK3.get_positive_time(options, :offset, 0)
+          if offset > interval
+            fail "Offset (#{offset}) must be less than or equal to interval (#{interval})"
+          end
+          @demands[name] =
+            demand_class.new(@event_queue, source, quantity, interval, offset)
+        end
+      end
     end
 
     def init_and_run(duration=nil)
