35bef05a37b41647a58fdc8868a4345abe86be38
Anthropohedron <Anthropohedron@users.noreply.github.com>
2017-09-24 21:13:47 -0400

    more defensive to avoid errors in later versions of yaml-cpp

--- cpp/src/config/convert.cpp
+++ cpp/src/config/convert.cpp
@@ -20,18 +20,26 @@ static inline void setNames(StringMap<T> &dict) {
 
 namespace YAML {
 
+static inline bool isScalar(const Node &node) {
+  return node.IsDefined() && node.IsScalar();
+}
+
+static inline bool isMap(const Node &node) {
+  return node.IsDefined() && node.IsMap();
+}
+
 bool convert<SK3C::Logger>::decode(const Node &node, SK3C::Logger &rhs) {
   Node file  = node["file"];
   Node dir   = node["dir"];
   Node limit = node["limit"];
-  rhs.file  =  file.IsScalar() ?  file.as<string>() : "";
-  rhs.dir   =   dir.IsScalar() ?   dir.as<string>() : "";
-  rhs.limit = limit.IsScalar() ? limit.as<double>()   :  0;
+  rhs.file  =  isScalar(file) ?  file.as<string>() : "";
+  rhs.dir   =   isScalar(dir) ?   dir.as<string>() : "";
+  rhs.limit = isScalar(limit) ? limit.as<double>() :  0;
   return true;
 }
 
 bool convert<SK3C::Variants>::decode(const Node &node, SK3C::Variants &rhs) {
-  if (!node.IsMap()) {
+  if (!isMap(node)) {
     rhs.task    = "Task";
     rhs.machine = "Machine";
     rhs.demand  = "Demand";
@@ -39,29 +47,29 @@ bool convert<SK3C::Variants>::decode(const Node &node, SK3C::Variants &rhs) {
     Node task    = node["task"];
     Node machine = node["machine"];
     Node demand  = node["demand"];
-    rhs.task    =    task.IsScalar() ?    task.as<string>() : "Task";
-    rhs.machine = machine.IsScalar() ? machine.as<string>() : "Machine";
-    rhs.demand  =  demand.IsScalar() ?  demand.as<string>() : "Demand";
+    rhs.task    =    isScalar(task) ?    task.as<string>() : "Task";
+    rhs.machine = isScalar(machine) ? machine.as<string>() : "Machine";
+    rhs.demand  =  isScalar(demand) ?  demand.as<string>() : "Demand";
   }
   return true;
 }
 
 bool convert<SK3C::Task>::decode(const Node &node, SK3C::Task &rhs) {
-  if (!node.IsMap()) return false;
+  if (!isMap(node)) return false;
   Node batch_size = node["batch_size"];
   Node batch_time = node["batch_time"];
   Node unit_time  = node["unit_time"];
   Node deficit    = node["deficit"];
   Node debit      = node["debit"];
-  if (!(batch_size.IsScalar() &&
-        (batch_time.IsScalar() || unit_time.IsScalar()))) {
+  if (!(isScalar(batch_size) &&
+        (isScalar(batch_time) || isScalar(unit_time)))) {
     return false;
   }
   rhs.batch_size = batch_size.as<double>();
-  rhs.batch_time = batch_time.IsScalar() ? batch_time.as<double>() :
+  rhs.batch_time = isScalar(batch_time) ? batch_time.as<double>() :
     unit_time.as<double>() * rhs.batch_size;
-  rhs.deficit = deficit.IsScalar() ? deficit.as<double>() :
-    debit.IsScalar() ? debit.as<double>() : 0;
+  rhs.deficit = isScalar(deficit) ? deficit.as<double>() :
+    isScalar(debit) ? debit.as<double>() : 0;
   return true;
 }
 
@@ -73,41 +81,41 @@ bool convert<SK3C::Machine>::decode(const Node &node, SK3C::Machine &rhs) {
 
 static inline bool isDemand(const Node &destination) {
   return !destination.IsDefined() ||
-    (destination.IsScalar() && destination.as<string>() == "external");
+    (isScalar(destination) && destination.as<string>() == "external");
 }
 
 bool convert<SK3C::Demand>::decode(const Node &node, SK3C::Demand &rhs) {
-  if (!node.IsMap()) return false;
+  if (!isMap(node)) return false;
   Node    supplier = node["supplier"];
   Node destination = node["destination"];
   Node quantity    = node["quantity"];
   Node interval    = node["interval"];
   Node offset      = node["offset"];
-  if (!supplier.IsScalar() ||
-      !quantity.IsScalar() ||
-      !interval.IsScalar() ||
+  if (!isScalar(supplier) ||
+      !isScalar(quantity) ||
+      !isScalar(interval) ||
       !isDemand(destination)) {
     return false;
   }
   rhs.name = supplier.as<string>();
   rhs.interval = interval.as<double>();
-  rhs.offset   = offset.IsScalar() ? offset.as<double>() : 0;
+  rhs.offset   = isScalar(offset) ? offset.as<double>() : 0;
   rhs.quantity = quantity.as<double>();
   return true;
 }
 
 static inline bool isPrereq(const Node &destination) {
-  return destination.IsScalar() && destination.as<string>() != "external";
+  return isScalar(destination) && destination.as<string>() != "external";
 }
 
 bool convert<SK3C::Prereq>::decode(const Node &node, SK3C::Prereq &rhs) {
-  if (!node.IsMap()) return false;
+  if (!isMap(node)) return false;
   Node supplier    = node["supplier"];
   Node destination = node["destination"];
   Node quantity    = node["quantity"];
   Node interval    = node["interval"];
-  if (!supplier.IsScalar() ||
-      !quantity.IsScalar() ||
+  if (!isScalar(supplier) ||
+      !isScalar(quantity) ||
       interval.IsDefined() ||
       !isPrereq(destination)) {
     return false;
@@ -138,9 +146,15 @@ static inline void splitDemands(const Node &nodes,
 }
 
 bool convert<SK3C::Config>::decode(const Node &node, SK3C::Config &rhs) {
-  if (!node.IsMap()) return false;
+  if (!isMap(node)) return false;
   rhs.logger = node["logger"].as<SK3C::Logger>();
-  rhs.variants = node["variants"].as<SK3C::Variants>();
+  if (isMap(node["variants"])) {
+    rhs.variants = node["variants"].as<SK3C::Variants>();
+  } else {
+    rhs.variants.task    = "Task";
+    rhs.variants.machine = "Machine";
+    rhs.variants.demand  = "Demand";
+  }
   rhs.tasks = node["tasks"].as<StringMap<SK3C::Task>>();
   setNames(rhs.tasks);
   rhs.machines = node["machines"].as<StringMap<SK3C::Machine>>();
